81fd53dc6b0fe32aa494a1cd43010a409b171243
bochard <mail@tenkyuu.dev>
2025-04-28 09:53:11 +0800

    added multiple error pages

--- files/index.php
+++ files/index.php
@@ -4,39 +4,39 @@ $urlPath = rtrim($urlPath, "/");
 $fullPath = $urlPath === "" ? "/" : $urlPath;
 
 if ($handle = opendir(".")) {
-    echo "<html>";
-    echo "<head><title>Index of $fullPath</title></head>";
-    echo "<body>";
-    echo "<h1>Index of $fullPath</h1><hr><pre>";
-    echo '<a href="../">../</a>' . PHP_EOL;
+  echo "<html>";
+  echo "<head><title>Index of $fullPath</title></head>";
+  echo "<body>";
+  echo "<h1>Index of $fullPath</h1><hr><pre>";
+  echo '<a href="../">../</a>' . PHP_EOL;
 
-    $items = [];
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != ".." && $entry != "index.php") {
-            $items[] = $entry;
-        }
+  $items = [];
+  while (false !== ($entry = readdir($handle))) {
+    if ($entry != "." && $entry != ".." && $entry != "index.php") {
+      $items[] = $entry;
     }
-    closedir($handle);
+  }
+  closedir($handle);
 
-    sort($items);
+  sort($items);
 
-    foreach ($items as $entry) {
-        $filePath = "./" . $entry;
-        $lastModified = date("d-M-Y H:i", filemtime($filePath));
-        $size = is_dir($filePath) ? "-" : filesize($filePath);
-        $link = htmlspecialchars($entry);
+  foreach ($items as $entry) {
+    $filePath = "./" . $entry;
+    $lastModified = date("d-M-Y H:i", filemtime($filePath));
+    $size = is_dir($filePath) ? "-" : filesize($filePath);
+    $link = htmlspecialchars($entry);
 
-        if (is_dir($filePath)) {
-            $link .= '/';
-        }
-
-        echo "<a href=\"$entry\">$link</a>" . str_repeat(" ", 50 - strlen($link)) . "$lastModified" . str_repeat(" ", 10 - strlen($size)) . "$size" . PHP_EOL;
+    if (is_dir($filePath)) {
+      $link .= '/';
     }
 
-    echo "</pre><hr>";
-    echo "</body>";
-    echo "</html>";
+    echo "<a href=\"$entry\">$link</a>" . str_repeat(" ", 50 - strlen($link)) . "$lastModified" . str_repeat(" ", 10 - strlen($size)) . "$size" . PHP_EOL;
+  }
+
+  echo "</pre><hr>";
+  echo "</body>";
+  echo "</html>";
 } else {
-    echo "Unable to open directory.";
+  echo "Unable to open directory.";
 }
 ?>
